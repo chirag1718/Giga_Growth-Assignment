0158ceac4776574d42c70e595d8c5572dc958a92
Chirag <chiragsonar1718@gmail.com>
2023-06-08 06:29:13 +0530

    added message controller

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,8 @@
 import epxress from "express";
 const router = epxress.Router();
-import { stockData } from "../controllers/stockController.js";
+import { message, stockData } from "../controllers/dashboardController.js";
 
 router.get("/", stockData);
+router.post("/message", message);
 
 export default router;
