933d0e8a67fa150904204056b39a0abd1dddd0e1
Chirag <chiragsonar1718@gmail.com>
2023-06-08 06:27:53 +0530

    feat: added message route logic and renamed file to dashboardController

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-
+import Message from "../model/Message.js";
 export const stockData = async (req, res) => {
   const apikey = process.env.STOCK_API_KEY;
   try {
@@ -12,5 +12,24 @@ export const stockData = async (req, res) => {
     res.status(200).send(stockData);
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const message = async (req, res) => {
+  try {
+    // create new message
+    const newMessage = await new Message({
+      fullName: req.body.fullName,
+      emailTo: req.body.emailTo,
+      emailFrom: req.body.emailFrom,
+      message: req.body.message,
+    });
+
+    const savedMessage = await newMessage.save();
+    console.log(savedMessage);
+    res.status(200).send(savedMessage);
+  } catch (err) {
+    res.status(400).send(err);
+    console.log(err, "Message save failed");
   }
 };
